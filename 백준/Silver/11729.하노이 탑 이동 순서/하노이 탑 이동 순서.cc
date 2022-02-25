e71a808932cdd34356dd2be621f3a39ab06c340c
HEEYEON LEE <58112670+heedd@users.noreply.github.com>
2022-02-26 04:00:09 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 120 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Silver/11729.하노이 탑 이동 순서/하노이 탑 이동 순서.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 원판 n개를 a기둥에서 b기둥으로 옮기는 방법 출력
+void func(int a, int b, int n){
+    if(n == 1) {
+        cout << a << ' ' << b << '\n';
+        return;
+    }
+    func(a, 6-a-b, n-1);  // n-1개 원판을 a -> (6-a-b)로 이동
+    cout << a << ' ' << b << '\n'; // n개 원판을 a -> b로 이동
+    func(6-a-b, b, n-1);  // n-1개 원판을 (6-a-b) -> b로 이동
+}
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int k;
+    cin >> k;
+    cout <<  (int)pow(2, k) -1 << '\n'; // 총 옮긴 횟수
+    //cout <<  (1<<k) - 1 << '\n'; // 총 옮긴 횟수
+    func(1, 3, k);  // 과정 출력하는 재귀 함수
+}
