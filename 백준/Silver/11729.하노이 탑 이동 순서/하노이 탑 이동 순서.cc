69a7fd096f1599a288530016554af339f8d95fc4
HEEYEON LEE <58112670+heedd@users.noreply.github.com>
2022-02-27 15:05:25 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 120 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11729.하노이 탑 이동 순서/하노이 탑 이동 순서.cc
+++ 백준/Silver/11729.하노이 탑 이동 순서/하노이 탑 이동 순서.cc
@@ -3,13 +3,16 @@ using namespace std;
 
 // 원판 n개를 a기둥에서 b기둥으로 옮기는 방법 출력
 void func(int a, int b, int n){
+    // base condition
     if(n == 1) {
         cout << a << ' ' << b << '\n';
         return;
     }
-    func(a, 6-a-b, n-1);  // n-1개 원판을 a -> (6-a-b)로 이동
-    cout << a << ' ' << b << '\n'; // n개 원판을 a -> b로 이동
-    func(6-a-b, b, n-1);  // n-1개 원판을 (6-a-b) -> b로 이동
+    
+    // 재귀식
+    func(a, 6-a-b, n-1);    // n-1개 원판을 a -> (6-a-b)로 이동
+    cout << a << ' ' << b << '\n';   // n개 원판을 a -> b로 이동
+    func(6-a-b, b, n-1);    // n-1개 원판을 (6-a-b) -> b로 이동
 }
 
 int main(void){
@@ -18,7 +21,9 @@ int main(void){
     
     int k;
     cin >> k;
-    cout <<  (int)pow(2, k) -1 << '\n'; // 총 옮긴 횟수
-    //cout <<  (1<<k) - 1 << '\n'; // 총 옮긴 횟수
+
+    cout <<  (1<<k) - 1 << '\n'; // 총 옮긴 횟수
+    //cout <<  (int)pow(2, k) -1 << '\n'; // 총 옮긴 횟수
+    
     func(1, 3, k);  // 과정 출력하는 재귀 함수
 }
