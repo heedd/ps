740347f09b80be29d7588874154a062953bfa601
HEEYEON LEE <58112670+heedd@users.noreply.github.com>
2022-05-13 00:52:24 +0900

    [Silver II] Title: MVP 다이아몬드 (Easy), Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/20413. MVP 다이아몬드 （Easy）/MVP 다이아몬드 （Easy）.py
+++ 백준/Silver/20413. MVP 다이아몬드 （Easy）/MVP 다이아몬드 （Easy）.py
@@ -1,23 +1,24 @@
 n = int(input())
 s, g, p, d = list(map(int, input().split()))
-tier = input()
+tier = list(input())
 sum = 0
 prev = 0
 
-for i in range(n):
-    if tier[i] == 'B':
-        sum += s - 1 - prev
-        prev = s - 1 - prev
-    elif tier[i] == 'S':
-        sum += g - 1 - prev
-        prev = g - 1 - prev
-    elif tier[i] == 'G':
-        sum += p - 1 - prev
-        prev = p - 1 - prev
-    elif tier[i] == 'P':
-        sum += d - 1 - prev
-        prev = d - 1 - prev
-    elif tier[i] == 'D':
+for t in tier:
+    if t == 'B':
+        sum += (s - 1) - prev
+        prev = (s - 1) - prev
+    elif t == 'S':
+        sum += (g - 1) - prev
+        prev = (g - 1) - prev
+    elif t == 'G':
+        sum += (p - 1) - prev
+        prev = (p - 1) - prev
+    elif t == 'P':
+        sum += (d - 1) - prev
+        prev = (d - 1) - prev
+    elif t == 'D':
         sum += d
         prev = d
+
 print(sum)
