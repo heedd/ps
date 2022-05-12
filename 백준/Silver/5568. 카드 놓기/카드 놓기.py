a550d150284cdcdd4ced0711a95070df007c34d3
HEEYEON LEE <58112670+heedd@users.noreply.github.com>
2022-05-12 21:39:32 +0900

    [Silver IV] Title: 카드 놓기, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/5568. 카드 놓기/카드 놓기.py
+++ 백준/Silver/5568. 카드 놓기/카드 놓기.py
@@ -5,19 +5,9 @@ n = int(input())
 k = int(input())
 num = [input().rstrip() for _ in range(n)]
 
-from itertools import combinations, permutations
+from itertools import permutations
 
-comb = list(combinations(num, k))
-select = []
-for item in comb :
-    tmp = sorted(item)
-    if tmp not in select  :
-        select.append(tmp)
-
-answer = []
-for item in select :
-    tmp = list(set(list(permutations(item))))
-    for i in tmp :
-        answer.append(''.join(i))
-
-print(len(set(answer)))
+answer = set()
+for item in permutations(num, k):
+     answer.add(''.join(item))
+print(len(answer))
