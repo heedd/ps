64b40511f9748173252759cfe82c9454d56bccdc
HEEYEON LEE <58112670+heedd@users.noreply.github.com>
2022-07-02 00:58:29 +0900

    [D2] Title: 파리 퇴치, Time: 164 ms, Memory: 59,040 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
@@ -1,18 +1,17 @@
 T = int(input())
 
-for test_case in range(1, T + 1):
-    n, m = map(int, input().split())
-    # 0,0 ~ 0,3
-    # 3,0 ~ 3,3
-    result = 0
-    grid = [list(map(int, input().split())) for _ in range(n)]
+for test_case in range(1, T+1):
+    N,M = map(int, input().split())
+    arr = [list(map(int,input().split())) for _ in range(N)]
 
+    result = []
+    for i in range(N-M+1):	# 최대 파리채 영역 탐색
+        for j in range(N-M+1):
+            kill = 0
+            for a in range(M):	# MxM 파리수 세기
+                for b in range(M):
+                    kill += arr[i+a][j+b]
 
-    for i in range(n-(m-1)) :
-        for j in range(n-(m-1)) :
-            tmp = 0
-            for k in range(m) :
-                for n in range(m) :
-                    tmp += grid[i+k][j+n]
-            result = max(result, tmp)
-    print('#'+str(test_case), result)
+            result.append(kill)
+
+    print('#'+str(test_case), max(result))
