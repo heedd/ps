94fbe44f5a79c17a29ac8ed56b08c7915e6439d8
HEEYEON LEE <58112670+heedd@users.noreply.github.com>
2022-08-01 21:41:35 +0900

    [Silver III] Title: 스위치 켜고 끄기, Time: 144 ms, Memory: 14620 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -1,50 +1,54 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
+//남(1) : 스위치번호가 배수이면 상태 반전
+//여(2) : 나 중심으로 좌우 대칭 구간
 public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int total = Integer.parseInt(br.readLine()); //스위치의 개수
-		int[] switches = new int[total];
-		StringTokenizer st = new StringTokenizer(br.readLine()); //스위치 string 받음
-		for(int i=0; i<total; i++)
+
+		// 1. 입력
+		int n = Integer.parseInt(br.readLine());
+		int[] switches = new int[n];
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++)
 			switches[i] = Integer.parseInt(st.nextToken());
-		
-		int stuCnt = Integer.parseInt(br.readLine()); //학생의 명수
-		for(int i=0; i<stuCnt; i++) {
+
+		// 2. switch 바꾸기
+		int m = Integer.parseInt(br.readLine());
+		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
-			int gender = Integer.parseInt(st.nextToken());
-			int number = Integer.parseInt(st.nextToken());
+			int gen = Integer.parseInt(st.nextToken());
+			int num = Integer.parseInt(st.nextToken());
+			int d = 1;
 			
-			//남학생이면
-			if(gender == 1) {
-				for(int j=0; j<total; j++) //뽑은 수의 배수 위치에 있는 스위치의 상태를 바꾼다.
-					if((j+1) % number == 0)
-						switches[j] = switches[j] == 0? 1: 0;
-			}
-			//여학생이면
-			else {
-				//뽑은 수를 중심으로 좌우가 대칭이면 상태를 바꾼다.
-				switches[number - 1] = switches[number - 1] == 0 ? 1 : 0;
-				for(int j=1; j<total/2; j++) {
-					if(number - 1 + j >= total || number - 1 - j < 0)
+			if (gen == 1) { // 남자일때
+				for (int j = 0; j < switches.length; j++) {
+					if((j+1)%num == 0)
+						switches[j] = (switches[j] == 0 ? 1 : 0);
+				}
+			} else { // 여자일때
+				switches[num-1] = (switches[num-1] == 0 ? 1 : 0);
+				while (0<=num-d-1 && num+d-1<n) {
+					if (switches[num-d-1] == switches[num+d-1]) {
+						switches[num-d-1] = (switches[num-d-1] == 0 ? 1 : 0);
+						switches[num+d-1] = switches[num-d-1];
+						d++;
+					} else
 						break;
-					if(switches[number - 1 - j] == switches[number - 1 + j]) {
-						switches[number - 1 - j] = switches[number - 1 - j] == 0 ? 1 : 0;
-						switches[number - 1 + j] = switches[number - 1 + j] == 0 ? 1 : 0;
-					}
-					else break; //대칭 아닌것이 나오면 바로 끝낸다.
 				}
 			}
 		}
-		
-		//한 줄에 20개씩 출력
-		for(int i=0; i<total; i++) {
+
+		// 3. 스위치 출력
+		for (int i = 0; i < switches.length; i++) {
 			System.out.print(switches[i] + " ");
-			if((i+1) % 20 == 0)
+			if ((i+1) % 20 == 0)
 				System.out.println();
 		}
 	}
+
 }
