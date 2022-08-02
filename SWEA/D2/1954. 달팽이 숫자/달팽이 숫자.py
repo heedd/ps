edb75fd13dfb0dfaa2af7ee6d0be7a573016f083
HEEYEON LEE <58112670+heedd@users.noreply.github.com>
2022-08-02 22:10:55 +0900

    [D2] Title: 달팽이 숫자, Time: 147 ms, Memory: 58,784 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
@@ -1,35 +1,65 @@
-T = int(input())
+import java.util.Scanner;
+import java.io.FileInputStream;
 
-#     0  1  2  3
-dy = (0, 1, 0, -1)
-dx = (1, 0, -1, 0)
+class Solution {
+	
+	//			인덱스       0  1  2  3
+	static int[] dy = {0, 1, 0, -1};
+	static int[] dx = {1, 0, -1, 0};
+	static int[][] grid;
+	static int n;
 
-for test_case in range(1, T + 1):
-    n = int(input())
-    board = [[0]*n for _ in range(n)]
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		
+		for(int test_case = 1; test_case <= T; test_case++) {
+			//1. 입력
+			n = sc.nextInt();
+			grid = new int[n][n];
+			
+			
+			//2. 달팽이 숫자
+			for (int i = 0; i < n; i++) {	// 1 2 3
+				grid[0][i] = i+1;
+			}
+			
 
-    for i in range(n) :
-        board[0][i] = i+1
-
-    way = []
-    s = 0
-    for i in range(n-1, 0, -1) :
-        for _ in range(2) :
-            s = (s+1) % 4
-            for _ in range(i) :
-                way.append(s)
-
-    y, x = 0, n-1
-    val = n
-    for i in way :
-        y, x = y + dy[i], x + dx[i]
-        val += 1
-        board[y][x] = val
-
-
-    print('#'+str(test_case))
-    for i in range(n) :
-        for j in range(n) :
-            print(board[i][j], end=' ')
-            if j == n-1 :
-                print()
+			int y = 0;
+			int x = n-1;
+			int d = 0;
+			int val = n+1;
+			for (int i = n-1; i > 0; i--) { // 4,5 - 6,7 - 8 - 9
+				d = (d+1)%4;
+				for (int k = 0; k < i; k++) {
+					y = y+dy[d];
+					x = x+dx[d];
+					grid[y][x] = val;
+					val++;
+				}
+				
+				d = (d+1)%4;
+				for (int k = 0; k < i; k++) {
+					y = y+dy[d];
+					x = x+dx[d];
+					grid[y][x] = val;
+					val++;
+				}
+			}
+			
+			//3. 출력
+		    System.out.print("#"+test_case+"\n");
+		    for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					System.out.print(grid[i][j]+" ");
+					if (j == n-1){
+						System.out.println();
+					}
+				}
+			}
+		    
+		    
+		}//end of tc
+	}
+}
