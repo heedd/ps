1bfb426d1fdf91ec9dddd560aab1314c114459f3
HEEYEON LEE <58112670+heedd@users.noreply.github.com>
2022-08-08 17:06:34 +0900

    [Gold V] Title: 탑, Time: 3180 ms, Memory: 127984 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,7 +1,5 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
@@ -9,28 +7,36 @@ public class Main {
 
     
     public static void main(String[] args) throws Exception {
+    	// 1. 입력
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        
         int n = Integer.parseInt(bf.readLine());
         StringTokenizer st = new StringTokenizer(bf.readLine());
         
         Stack<int[]> s = new Stack<>();
-        int[] cur = new int[2];
-     
-        for(int i = 1; i <= n; i++) {
-            cur[0] =  Integer.parseInt(st.nextToken());
-            cur[1] = i;
-            while(!s.isEmpty()) {
-            	if(cur[0] < s.peek()[0]) {
-            		System.out.print(s.peek()[1]+" ");
-            		break;
-            	}
-            	s.pop();
+        int curHeight = 0;
+        int[] answer = new int[n];
+        
+        // 2. 신호 송수신
+        for(int i = 1; i <= n; i++) {	// 탑의 번호 1,2...,n이므로
+            curHeight = Integer.parseInt(st.nextToken());
+            // 스택의 마지막에 들어간 값이 input보다 작으면 계속 pop
+            while(!s.isEmpty() && s.peek()[0] < curHeight) {
+            	// curHeight보다 크면 수신받음
+            		s.pop();
             }
-            if(s.isEmpty()) {
-            	System.out.print("0 ");	//아무도
+            
+            // 스택비어있으면 아무도 curHeight의 신호 수신받을 수 없음
+            if(!s.isEmpty()) {
+            	answer[i-1] = s.peek()[1];
+            }else{
+            	answer[i-1] = 0;
             }
-            s.push(Arrays.copyOf(cur, 2));
+            s.push(new int[]{curHeight, i});	//참조형 변수 넘겨주면 안됨! pass by value
+        }
+        
+        //3. 출력
+        for(int a:answer) {
+        	System.out.print(a+" ");
         }
     }
 }
