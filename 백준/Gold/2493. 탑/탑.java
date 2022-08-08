fb871b7ef1f6af5a9489c88819f82401fbcb6697
HEEYEON LEE <58112670+heedd@users.noreply.github.com>
2022-08-08 14:24:26 +0900

    [Gold V] Title: 탑, Time: 3208 ms, Memory: 126888 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,18 +1,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
-    static class Top {
-		int height;
-    	int idx;
-    	public Top(int height, int idx) {
-    		this.height = height;
-    		this.idx = idx;
-    	}
-    }
+
     
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -20,23 +14,23 @@ public class Main {
         int n = Integer.parseInt(bf.readLine());
         StringTokenizer st = new StringTokenizer(bf.readLine());
         
-        Stack<Top> s = new Stack<>();
-        
+        Stack<int[]> s = new Stack<>();
+        int[] cur = new int[2];
      
         for(int i = 1; i <= n; i++) {
-            int height = Integer.parseInt(st.nextToken());
-            
+            cur[0] =  Integer.parseInt(st.nextToken());
+            cur[1] = i;
             while(!s.isEmpty()) {
-            	if(height < s.peek().height) {
-            		System.out.print(s.peek().idx+" ");
+            	if(cur[0] < s.peek()[0]) {
+            		System.out.print(s.peek()[1]+" ");
             		break;
             	}
             	s.pop();
             }
             if(s.isEmpty()) {
-            	System.out.print("0 ");
+            	System.out.print("0 ");	//아무도
             }
-            s.push(new Top(height, i));
+            s.push(Arrays.copyOf(cur, 2));
         }
     }
 }
