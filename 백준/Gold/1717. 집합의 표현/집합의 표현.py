58934748a379c9a79c00bd4e775bb8cf0760fe9e
HEEYEON LEE <58112670+heedd@users.noreply.github.com>
2022-08-21 03:06:52 +0900

    [Gold IV] Title: 집합의 표현, Time: 340 ms, Memory: 284716 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.py
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -1,37 +1,38 @@
+# 유니온파인드
 import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(100000)
+
+#find : x의 루트노트를 찾자. (+ 찾으면서 루트노드로 parents배열 완성하기)
+def find(parents, x):
+    if parents[x] != x:                          #루트노드면 같을텐데 아니니까..
+        parents[x] = find(parents, parents[x])   #x의 부모인 parents[x]의 부모에대해 재귀적으로 찾자..
+    return parents[x]                            #parents[x]==x 루트노드네! 루트노드 리턴
+
+#union
+def union(parents, a, b):
+    a = find(parents, a)
+    b = find(parents, b)
+    if  a < b:
+        parents[b] = a
+    else:
+        parents[a] = b
+
 n, m = map(int, input().split())
-
-# union : 두 집합을 하나의 집합으로 합치는 연산 (== 루트노드를 하나로 합친다)
-def union(x, y) :
-    x = find(x) #x와 y의 루트노드를 찾아준다.
-    y = find(y)
-    if x == y: #루드가 같다면 합칠 필요가 없으니 종료
-        return
-    parent[x] = y #x의 부모를 y로 만들어 합쳐주기
-    return
-
-# union : 어떤 원소 x가 속한 집합을 반환 (== x의 루트노드를 반환)
-def find(x) :
-    if parent[x] == x : # x가 루트노드이면 자기 자신 반환
-        return x
-    parent[x] = find(parent[x]) # x가 루트가 아니라면 재귀를 이용해 루트노드를 찾는다.
-    return parent[x]
-
-parent = [0]*(n+1)
-for i in range(n+1) :
-    parent[i] = i
+#인덱스노드의 루트노드가 담길 parent배열
+parents = [i for i in range(n+1)]  #인덱스 0부터 시작
 
 
-for i in range(m) :
-    x, a, b = map(int, input().split())
-    if x == 0 :
-        union(a,b)
-    else :
-        pa = find(a)
-        pb = find(b)
-        if pa == pb:
-            print('YES')
+
+for _ in range(m):
+    op, a, b = map(int, input().split())
+    if op == 0:
+        union(parents, a, b)
+    else:
+        pa = find(parents, a)
+        pb = find(parents, b)
+
+        if pa==pb:
+            print("YES")
         else:
-            print('NO')
+            print("NO")
