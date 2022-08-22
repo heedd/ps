e1756ef56d484a171942d4eb20a51b0de853fb71
HEEYEON LEE <58112670+heedd@users.noreply.github.com>
2022-08-22 19:13:49 +0900

    [D4] Title: [S/W 문제해결 기본] 10일차 - Contact, Time: 115 ms, Memory: 20,272 KB -BaekjoonHub

--- SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.java
+++ SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.java
@@ -6,17 +6,21 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static int n, start;
-	static int[][] graph;
-	static boolean[] visited;
+	static int n, start;		//간선 개수, 시작점
+	static int[][] graph;		//인접행렬 - bfs돌면서 time정보 갱신해줄 것임
+	static boolean[] visited;	//방문check
 	static int time;
 	public static void main(String[] args) throws NumberFormatException, IOException {
+//		BufferedReader bf = new BufferedReader(new FileReader("res/input.txt"));
+
+		// 1. 입력
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		for (int tc = 1; tc <= 10; tc++) {
 			st = new StringTokenizer(bf.readLine());
 			n = Integer.parseInt(st.nextToken());
 			start = Integer.parseInt(st.nextToken());
+			//노드 번호 1~100
 			graph = new int[101][101];
 			visited = new boolean[101];
 			st = new StringTokenizer(bf.readLine());
@@ -27,8 +31,11 @@ public class Solution {
 				graph[from][to] = 1;
 			}
 
+			// 2. 레벨단위 bfs
 			bfs(start);
 			
+			
+			// 3. 출력 - graph[from][to]==time 인 인덱스to 중 가장 큰 인덱스 출력
 			int last = 0;
 			for (int i = 0; i <= 100; i++) {
 				for (int j = 0; j <= 100; j++) {
@@ -51,14 +58,17 @@ public class Solution {
 		time = 0;
 		while(!q.isEmpty()) {
 			time+=1;
+			//레벨(time)단위 bfs - 같은 시간에 동시다발적으로 연락이 가므로
 			int end = q.size();
 			for (int i = 0; i < end; i++) {
 				cur = q.poll();
 				for (int ad = 1; ad <= 100; ad++) {
-					if(visited[ad] || graph[cur][ad]==0) continue;
+					// cur와 연결되지 않은 노드거나, 이미 방문했다면 continue
+					if(graph[cur][ad]==0 || visited[ad]) continue;
+					// 연락받아야될 노드면, Q에 넣고-방문표시하고-그래프에 time정보 갱신
 					q.offer(ad);
-					graph[cur][ad] += time;
 					visited[ad] = true;
+					graph[cur][ad] += time;
 				}
 			}
 		}
