2eb445eae273cce8bdf341bfdf7738f823f3a561
HEEYEON LEE <58112670+heedd@users.noreply.github.com>
2022-10-09 21:30:07 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 224 ms, Memory: 132516 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.py
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.py
@@ -1,16 +1,31 @@
-N, M = map(int, input().split())
-A = []
-for i in range(N):
-    s = input()
-    A.append(list(map(int, list(s))))
+"""
+- 문제 : 가장 큰 정사각형
+- 링크 : https://www.acmicpc.net/problem/1915
+- 아이디어 : DP
+- 시간복잡도 : 1000*1000 n^2이지 않을까
+"""
+# import sys
+# input = sys.stdin.readline
 
-# A 탐색하면서 채울 DP 테이블
-dp = [[0] * (M+1) for _ in range(N+1)]
-size = 0    # 최대 변의 길이
+n, m = map(int, input().split())
+# 인덱스 1부터 진짜
+grid = []
+for i in range(n):
+    line = input()
+    grid.append(list(map(int, list(line))))
 
-for i in range(1, N+1):
-    for j in range(1, M+1):
-        if A[i-1][j-1]:
-            dp[i][j] = min(dp[i][j-1], dp[i-1][j], dp[i-1][j-1]) + 1
-            size = max(size, dp[i][j])
-print(size ** 2)
+    """
+    시작점: 좌상, 끝점: 우하
+    "좌|좌상|상" 값을 비교하자
+    dp[er][ec] = sz : (er,ec)인 가장 큰 정사각형의 크기
+    """
+# 인덱스 1부터 진짜
+mxLength = 0
+dp = [[0]*(m+1) for _ in range(n+1)]
+for r in range(1, n+1):
+    for c in range(1, m+1):
+        if grid[r-1][c-1] == 1:
+            dp[r][c] = min(dp[r][c-1], dp[r-1][c-1], dp[r-1][c])+1
+            mxLength = max(mxLength, dp[r][c])
+
+print(mxLength*mxLength)
